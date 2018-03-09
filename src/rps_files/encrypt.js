5947c9f8f0e76a8d43091bc63f2aebc821df21c1
smjnab <web@jensnilsson.com>
2018-03-09 10:57:20 +0100

    Request encrypted game choices from server
    Server uses submitted data + salt to create md5 sums of player choices. API endpoints and server backend has to be created if wanting to use this.

---
+++ src/rps_files/encrypt.js
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// ENCRYPT AND VERIFY MD5 FOR RPS CHOICES
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function MakeMD5(type, user, permlink, rpschoice, rpschoicemd5 = "") {
+
+    /// Enable caching.
+    $.ajaxSetup({
+        cache: true
+    });
+
+    /// Send request to backend lambda function. 
+    var lambdaURL = "";
+
+    /// Pick API endpoint for live or dev site.
+    if (window.location.href.includes("dev.spelmakare.se")) lambdaURL = "https://fjlhwlra2d.execute-api.eu-west-1.amazonaws.com/dev/md5";
+    else lambdaURL = "https://8gd2qvvsn7.execute-api.eu-west-1.amazonaws.com/live/md5";
+
+    return $.ajax({
+        url: lambdaURL,
+        method: "post",
+        dataType: "json",
+        data: JSON.stringify({
+            type: type, ///create or verify.
+            user: user, ///name of user making post.
+            permlink: permlink, ///permlink to post.
+            rpschoice: rpschoice, ///choice made for RPS.
+            rpschoicemd5: rpschoicemd5 ///optional md5, used if type is verify.
+        })
+    });
+}
